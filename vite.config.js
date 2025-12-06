a652f7b1045e5fc15b848d4e549f5a628cc4bc51
SandaluThisanka <sandaluthisankaa@gmail.com>
2025-12-06 22:23:50 +0530

    Fix GH Pages build output

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/Portfolio-sandalu/',
+  base: '/Sandalu_Portfolio/',
   plugins: [react()],
 });
